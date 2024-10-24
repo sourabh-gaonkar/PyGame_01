1dbcb080b2472053e9b670c8c0ee0b440ea026ca
Sourabh Gaonkar <sourabh.gaonkar@intimetec.com>
2024-10-24 12:30:46 +0530

    enemier part I

--- .venv/src/level.py
+++ .venv/src/level.py
@@ -7,6 +7,7 @@ from support import *
 from random import choice
 from weapon import Weapon
 from ui import UI
+from enemy import Enemy
 
 class Level:
     def __init__(self):
@@ -30,7 +31,8 @@ class Level:
         layouts = {
             'boundary': import_csv_layout('.venv/map/map_FloorBlocks.csv'),
             'grass': import_csv_layout('.venv/map/map_Grass.csv'),
-            'object': import_csv_layout('.venv/map/map_Objects.csv')
+            'object': import_csv_layout('.venv/map/map_Objects.csv'),
+            'entities': import_csv_layout('.venv/map/map_Entities.csv')
         }
 
         graphics = {
@@ -52,15 +54,23 @@ class Level:
                         if style == 'object':
                             surf = graphics['objects'][int(col)]
                             Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
+                        if style == 'entities':
+                            if col == '394':
+                                self.player = Player(
+                                    (x, y),
+                                    [self.visible_sprites],
+                                    self.obstacle_sprites,
+                                    self.create_attack,
+                                    self.destroy_attack,
+                                    self.create_magic
+                                )
+                            else:
+                                if col == '390': monster_name = 'bamboo'
+                                elif col == '391': monster_name = 'spirit'
+                                elif col == '392': monster_name = 'raccoon'
+                                else: monster_name = 'squid'
 
-        self.player = Player(
-            (2000, 1430),
-            [self.visible_sprites],
-            self.obstacle_sprites,
-            self.create_attack,
-            self.destroy_attack,
-            self.create_magic
-        )
+                                Enemy(monster_name, (x, y), [self.visible_sprites])
 
     def create_attack(self):
         self.current_attack = Weapon(self.player, [self.visible_sprites])
